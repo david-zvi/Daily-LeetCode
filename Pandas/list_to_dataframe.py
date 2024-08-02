4d5995827ddba8897076c36b32afbd946d838a0f
david-zvi <dzk345@gmail.com>
2024-08-02 20:40:09 +0300

    2877. Create a DataFrame from List

---
+++ Pandas/list_to_dataframe.py
@@ -0,0 +1,7 @@
+# Solved on 02/08/2024
+# https://leetcode.com/problems/create-a-dataframe-from-list/
+
+import pandas as pd
+
+def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
+    return pd.DataFrame(student_data, columns=["student_id", "age"])
