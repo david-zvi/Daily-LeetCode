4453bce2e2ff03edde5166c52539ee382086d2f9
david-zvi <dzk345@gmail.com>
2024-08-11 10:35:28 +0300

    2886. Change Data Type

---
+++ Pandas/change_data_type.py
@@ -0,0 +1,8 @@
+# Solved on 11/08/2024
+# https://leetcode.com/problems/change-data-type/
+
+import pandas as pd
+
+def changeDatatype(students: pd.DataFrame) -> pd.DataFrame:
+    students["grade"] = students["grade"].astype(int)
+    return students
